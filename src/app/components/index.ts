ebb30df834c4114fd81a9193b767508f7e4ade95
Karim Beiranvand <karim@Karims-MacBook-Pro.local>
2023-01-22 16:27:14 +0100

    game scoring history card

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,4 @@
 export { FramePerformanceReceivingFormComponent } from './frame-performance-receiving-form/frame-performance-receiving-form.component'
 export { GameScoringHistoryTableComponent } from './game-scoring-history-table/game-scoring-history-table.component'
+export { GameScoringHistoryCardComponent } from './game-scoring-history-card/game-scoring-history-card.component'
 export { FrameHitsFormCardComponent } from './frame-hits-form-card/frame-hits-form-card.component'
