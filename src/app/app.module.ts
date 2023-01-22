2c3397f2ab67f45e1329c7cda1f3b1496a04cbec
Karim Beiranvand <karim@Karims-MacBook-Pro.local>
2023-01-22 15:59:19 +0100

    form to input frame performance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { StoreModule } from '@ngrx/store'
-import { NgModule } from '@angular/core'
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
+import { FramePerformanceReceivingFormComponent } from './components'
 import { HomePageComponent, NotFoundPageComponent } from './pages'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { framesReducer } from './reducers'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, NotFoundPageComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    NotFoundPageComponent,
+    FramePerformanceReceivingFormComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     StoreModule.forRoot({ frames: framesReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
