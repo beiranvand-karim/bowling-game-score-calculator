2fb4f18eef71674b1a3366a5e56523c30d88f220
Karim Beiranvand <beiranvand.karim@gmail.com>
2023-01-22 16:06:46 +0100

    frame form containing card (#9)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { StoreModule } from '@ngrx/store'
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatButtonModule } from '@angular/material/button'
+import { BrowserModule } from '@angular/platform-browser'
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { StoreModule } from '@ngrx/store'
 
-import { FramePerformanceReceivingFormComponent } from './components'
 import { HomePageComponent, NotFoundPageComponent } from './pages'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { framesReducer } from './reducers'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatButtonModule } from '@angular/material/button'
+import {
+  FramePerformanceReceivingFormComponent,
+  FrameHitsFormCardComponent,
+} from './components'
 
 @NgModule({
   declarations: [
@@ -18,16 +22,18 @@ import { MatButtonModule } from '@angular/material/button'
     HomePageComponent,
     NotFoundPageComponent,
     FramePerformanceReceivingFormComponent,
+    FrameHitsFormCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({ frames: framesReducer }),
     ReactiveFormsModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    StoreModule.forRoot({ frames: framesReducer }),
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
