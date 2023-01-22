0d08f376ee0cb9004e40fd0b108e0b06b6a3a8f5
Karim Beiranvand <karim@Karims-MacBook-Pro.local>
2023-01-22 16:46:05 +0100

    put everything together in home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core'
+import { map, Observable } from 'rxjs'
+import { Store } from '@ngrx/store'
+
+import { AppState, ScoreCalculatorService } from '../../declarations'
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  allPlayedFramesScore: Observable<number>
+
+  constructor(
+    private store: Store<AppState>,
+    private scoreCalculatorService: ScoreCalculatorService
+  ) {
+    this.allPlayedFramesScore = this.store
+      .select(state => state.frames)
+      .pipe(
+        map(frames => this.scoreCalculatorService.calculateTotalScore(frames))
+      )
+  }
+}
