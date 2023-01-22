22c707916b3c330a9800c712815e3d05fe9d4502
Karim Beiranvand <beiranvand.karim@gmail.com>
2023-01-22 14:26:15 +0100

    reducer (#6)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser'
+import { StoreModule } from '@ngrx/store'
 import { NgModule } from '@angular/core'
 
 import { HomePageComponent, NotFoundPageComponent } from './pages'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { framesReducer } from './reducers'
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, NotFoundPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ frames: framesReducer }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
