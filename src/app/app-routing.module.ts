0eb61f15c53cff9ff89632a080c7e0703788cd7b
Karim Beiranvand <karim@Karims-MacBook-Pro.local>
2023-02-08 15:06:34 +0100

    test packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
 import { HomePageComponent, NotFoundPageComponent } from './pages'
 
+import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core'
+
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
   { path: '**', component: NotFoundPageComponent },
+  { path: '', component: HomePageComponent },
 ]
 
 @NgModule({
